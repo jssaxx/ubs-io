cd955285a74731d74bd76ee001a19a716ea29f66
ljj929 <499109299@qq.com>
2024-01-22 15:32:58 +0800

    【修改描述】支持节点信息上报 【DTS/CR/AR单号】AR20240120617664 【合入版本】BeiMing-K for BigData 【修改点】支持节点信息上报
    Match-id-964f6b7c0d0b5e01caf729f3ab23e08320f5c860

---
+++ src/cluster/client/cm_client_init.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
+ */
+
+#ifndef CM_CLIENT_INIT_H
+#define CM_CLIENT_INIT_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int32_t CM_ClientInit(void);
+
+void CM_ClientExit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
