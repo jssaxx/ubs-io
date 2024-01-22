ebc9d2a3ed1a81432cffa00232a071dc375f6d07
user <user@domain.com>
2024-01-22 16:36:15 +0800

    【修改描述】支持写Cache淘汰缓存数据 【DTS/CR/AR单号】AR20240119611694、AR20240119611324、AR20240119596130、AR20240119611694 【合入版本】BeiMing-K 24.0.0
    Match-id-236ee970d05b3c8e3904269c15f09f4588369a1d

---
+++ src/cache/write/wcache.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+ */
+
+#ifndef BOOSTIO_WCACHE_H
+#define BOOSTIO_WCACHE_H
+
+#include <set>
+#include <unordered_map>
+#include "bio_execution.h"
+#include "bio_ref.h"
+#include "cache_def.h"
+#include "cache_slice.h"
+#include "cache_slice_operator.h"
+#include "flow.h"
+#include "underfs.h"
+#include "wcache_tier.h"
+#include "rcache_manager.h"
+
+namespace ock {
+namespace bio {
+class WCache {
+public:
+    using EvictCallback = std::function<BResult(uint64_t ptId, const Key &key)>;
+    BResult Init(uint64_t flowId, const ExecutorServicePtr &exeService, EvictCallback evictCallback);
+
+    void Exit();
+
+    BResult GetWCacheSlice(const SliceKey &sliceKey, WCacheSlicePtr &slice);
+
+    BResult Put(const Key &key, const WCacheSlicePtr &srcSlice, const SliceReader &sliceReader,
+        WCacheSliceRefPtr &destSliceRef);
+
+    BResult EvictAllMemSliceToDisk();
+
+    BResult EvictAllDiskSliceToUnderFs(const RCacheManagerPtr &rCacheManager);
+
+    uint64_t GetCapacity();
+
+    DEFINE_REF_COUNT_FUNCTIONS;
+private:
+    BResult EvictFromMemToDisk(WCacheSliceRefPtr &slice);
+    BResult EvictFromDiskToUnderFs(const RCacheManagerPtr &rCacheManager, const WCacheSliceRefPtr &sliceRef);
+
+private:
+    uint64_t mFlowId;
+    EvictCallback mEvictCallback;
+
+    WCacheTierPtr mCacheTiers[MAX_WCACHE_TIER];
+
+    CacheSliceOperator mSliceOperator;
+
+    ExecutorServicePtr mExeService;
+
+    UnderFsPtr mUnderFs;
+
+    DEFINE_REF_COUNT_VARIABLE;
+};
+using WCachePtr = Ref<WCache>;
+}
+}
+
+
+#endif // BOOSTIO_WCACHE_H
