35906ed8ae100258615bda301dd08740214bd0ba
user <user@domain.com>
2024-01-22 16:37:26 +0800

    【修改描述】支持广播分区视图 【DTS/CR/AR单号】AR20240120617836 【合入版本】BeiMing-K for BigData 【修改点】支持广播分区视图
    Match-id-531db5a3010c53509578380c4d3b9596a65c742c

---
+++ src/cluster/common/cm_inner.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
+ */
+
+#include "cm_server_init.h"
+#include "cm_client_init.h"
+#include "cm_module.h"
+#include "cm_config.h"
+#include "cm_zkadapter.h"
+
+static MODULE_DEFINE_S g_cmModules[] = {
+    { "cm_server_init",          CM_ServerInit,             CM_ServerExit    },
+    { "cm_client_init",          CM_ClientInit,             CM_ClientExit    },
+};
+
+int32_t CM_Init(ConfigRole role, PoolInfo *pools, uint16_t num, const char *zkIpMask, const char *ipStr)
+{
+    int32_t ret;
+
+    ret = CmConfigInit(role, pools, num, zkIpMask, ipStr);
+    if (ret != 0) {
+        return ret;
+    }
+
+    ret = CmZkInit();
+    if (ret != 0) {
+        return ret;
+    }
+
+    ret = INIT_MODULES(g_cmModules);
+    return ret;
+}
+
+void CM_Exit(void)
+{
+    EXIT_MODULES(g_cmModules);
+}
+
---
+++ src/cluster/server/pt/cm_pt_store.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2025. All rights reserved.
+ */
+
+#ifndef CM_PT_STORE_H
+#define CM_PT_STORE_H
+
+#include "cm_comm.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void *Storer;
+
+typedef struct {
+    Storer (*createStorer)(uint16_t maxNodeNum, uint16_t maxPtNum, uint16_t copyNum);
+    void (*destoryStorer)(Storer storer);
+    int32_t (*initial)(Storer storer, PtEntryList *ptList);
+    int32_t (*loadcheck)(Storer storer, PtEntryList *ptList);
+    int32_t (*update)(Storer storer, PtEntryList *ptList);
+} StoreOps;
+
+StoreOps *CmPtStoreOpsGet(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
