0806f2ffe35ca384ae8cc0ef2e117bf7e9a4df3c
user <user@domain.com>
2024-01-22 18:58:40 +0800

    【修改描述】支持线性空间数据读写 【DTS/CR/AR单号】AR20240120617969/AR20240120617969/AR20240120617937 【合入版本】BeiMing-K for BigData 【修改点】支持线性空间数据读写
    Match-id-1f26594256bd1513f7ecf6bbec6d04362d99d8f5

---
+++ src/flow/flow_task_pool.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
+ */
+#ifndef BOOSTIO_FLOW_TASK_POOL_H
+#define BOOSTIO_FLOW_TASK_POOL_H
+
+#include "bio_execution.h"
+#include "bio_err.h"
+
+namespace ock {
+namespace bio {
+using FlowTaskHandler = std::function<void(void)>;
+class FlowTask : public Runnable {
+public:
+    explicit FlowTask(FlowTaskHandler &handler) : mHandler(handler) {}
+
+    ~FlowTask() override = default;
+
+    void Run() override
+    {
+        mHandler();
+    }
+
+protected:
+    FlowTaskHandler mHandler;
+    friend class FlowTaskPool;
+};
+
+class FlowTaskPool {
+public:
+    explicit FlowTaskPool(const std::string &name)
+    {
+        /* thread name should be less than 16 chars */
+        mName = "Evt" + name;
+    }
+
+    ~FlowTaskPool()
+    {
+        Stop();
+    }
+
+    BResult Start(uint32_t coreThreadNum, uint32_t queueSiz);
+
+    void Stop();
+
+    BResult AddTask(FlowTaskHandler &handler);
+
+    DEFINE_REF_COUNT_FUNCTIONS;
+private:
+    void StopInner()
+    {
+        if (mExeService != nullptr) {
+            mExeService->Stop();
+            mExeService = nullptr;
+        }
+    };
+
+private:
+    DEFINE_REF_COUNT_VARIABLE;
+
+    ExecutorServicePtr mExeService { nullptr };
+    std::mutex mMutex;
+    bool mStarted = false;
+    std::string mName;
+};
+using FlowTaskPoolPtr = Ref<FlowTaskPool>;
+}
+}
+
+#endif // BOOSTIO_FLOW_TASK_POOL_H
+
