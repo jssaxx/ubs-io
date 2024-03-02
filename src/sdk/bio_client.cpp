8addac25d51d9a68a1963f67a3900c80bbf77a28
user <user@domain.com>
2024-03-02 17:21:03 +0800

    【修改描述】补充cli,解决DTS2024030102381，DTS2024030102231 【DTS/CR/AR单号】 #148 【合入版本】BeiMing-K 24.0.0
    Match-id-8167a91fa28f0164d3ccd4568c1e1837f87d0d5b

--- src/sdk/bio_client.cpp
+++ src/sdk/bio_client.cpp
@@ -2,11 +2,14 @@
  * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
  */
 
+#include <dlfcn.h>
 #include "bio_client_log.h"
 #include "bio_client_net.h"
 #include "bio_client_agent.h"
 #include "bio_client.h"
-
+#ifdef USE_DEBUG_TOOLS
+#include <dlfcn.h>
+#endif
 using namespace ock::bio;
 
 BResult BioClient::BioLoggerInit(BioService::WorkerMode mode)
@@ -15,6 +18,71 @@ BResult BioClient::BioLoggerInit(BioService::WorkerMode mode)
     auto logMode = static_cast<int32_t>(mode);
     return BioClientLog::Instance()->Initialize(logMode, defaultLogLevel);
 }
+
+#ifdef USE_DEBUG_TOOLS
+#define SDK_DIAGNOSE_PID 123
+using SdkDiagnose = int (*)();
+
+BResult BioClient::BioDiagnoseSdkInit()
+{
+    const char *soFileName = "libsdk_diagnose.so";
+    void *handler = dlopen(soFileName, RTLD_NOW);
+    if (handler == nullptr) {
+        CLIENT_LOG_ERROR("Failed to open library() " << soFileName << " dlopen , error " << dlerror());
+        return BIO_ERR;
+    }
+    SdkDiagnose sdkInitFunc = reinterpret_cast<SdkDiagnose>(dlsym(handler, "SdkDiagnoseInit"));
+    BResult ret = sdkInitFunc();
+    if (ret != BIO_OK) {
+        CLIENT_LOG_ERROR("Failed to Initialize sdk diagnose, ret:" << ret << ".");
+        return BIO_ERR;
+    }
+    return ret;
+}
+
+BResult BioClient::BioDiagnoseHtracerInit()
+{
+    const char *soFileName = "libhtracer_diagnose.so";
+    void *handler = dlopen(soFileName, RTLD_NOW);
+    if (handler == nullptr) {
+        CLIENT_LOG_ERROR("Failed to open library() " << soFileName << " dlopen , error " << dlerror());
+        return BIO_ERR;
+    }
+    SdkDiagnose sdkInitFunc = reinterpret_cast<SdkDiagnose>(dlsym(handler, "HtracerDiagnoseInit"));
+    BResult ret = sdkInitFunc();
+    if (ret != BIO_OK) {
+        CLIENT_LOG_ERROR("Failed to Initialize htracer diagnose, ret:" << ret << ".");
+        return BIO_ERR;
+    }
+    return ret;
+}
+
+BResult BioClient::BioClientDiagnoseInit(BioService::WorkerMode mode)
+{
+    BResult ret = BIO_OK;
+    if (mode == BioService::SEPARATES) {
+        char rootName[] = "bio_sdk";
+        ret =  CLI_AgentInit(SDK_DIAGNOSE_PID, rootName);
+        if (ret != BIO_OK) {
+            return BIO_ERR;
+        }
+    }
+
+    ret = this->BioDiagnoseSdkInit();
+    if (ret != BIO_OK) {
+        CLIENT_LOG_ERROR("Failed to Initialize sdk diagnose, ret:" << ret << ".");
+        return BIO_ERR;
+    }
+
+    ret = this->BioDiagnoseHtracerInit();
+    if (ret != BIO_OK) {
+        CLIENT_LOG_ERROR("Failed to Initialize htracer diagnose, ret:" << ret << ".");
+        return BIO_ERR;
+    }
+
+    return ret;
+}
+#endif
 
 BResult BioClient::Start(BioService::WorkerMode mode)
 {
@@ -38,6 +106,14 @@ BResult BioClient::Start(BioService::WorkerMode mode)
         CLIENT_LOG_ERROR("Failed to Initialize agent, ret:" << ret << ".");
         return ret;
     }
+
+#ifdef USE_DEBUG_TOOLS
+    ret = this->BioClientDiagnoseInit(mode);
+    if (ret != BIO_OK) {
+        CLIENT_LOG_ERROR("Failed to Initialize sdk diagnose.");
+        return ret;
+    }
+#endif
 
     net::BioClientNetPtr netEngine = net::BioClientNet::Instance();
     if (netEngine == nullptr) {
