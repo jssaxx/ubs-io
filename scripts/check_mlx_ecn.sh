ec39a3b86ad77dd58295031220c93d50aa208889
ljj929 <499109299@qq.com>
2024-03-15 09:47:23 +0800

    【修改描述】修改无损脚本，根据cx卡插入口配置 【DTS/CR/AR单号】#201 【合入版本】BeiMing 24.2.RC3
    Match-id-4ef69ca4f7299827a24f28668c8cdffa878b4ab2

--- scripts/check_mlx_ecn.sh
+++ scripts/check_mlx_ecn.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
   
-net_card=$(ibdev2netdev | grep mlx5_0 | awk '{print $5}')
+net_cards=$(ibdev2netdev | grep Up | awk '{print $5}')
 
-cma_roce_tos -d mlx5_0 -t 106
-#cma_roce_tos -d mlx5_1 -t 106
+for net_card in $net_cards
+do
+net_port=$(ibdev2netdev | grep $net_card | awk '{print $1}')
+cma_roce_tos -d $net_port -t 106
 mlnx_qos -i ${net_card} --pfc 0,0,0,1,0,0,0,0 --trust dscp
 #mlnx_qos -i ${net_card} --pfc 0,0,0,1,0,0,0,0
 ifconfig ${net_card} mtu 4500
@@ -27,3 +29,4 @@ if [ $roce_rp_3 != '1' ]; then
   echo "roce_rp_3:"$roce_rp_3
   echo 1 >/sys/class/net/${net_card}/ecn/roce_rp/enable/3
 fi
+done
