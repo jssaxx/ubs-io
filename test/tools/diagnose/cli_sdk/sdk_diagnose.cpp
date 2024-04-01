9ba62d8a3fe41edd3e96140deda7a9a5471ca006
ljj929 <499109299@qq.com>
2024-04-01 17:14:50 +0800

    【修改描述】删除htracer diagnose，serverdiagnose增加show trace 【DTS/CR/AR单号】 #257 【合入版本】BeiMing-K 24.0.0
    Match-id-645b46310486aa6a5459fde56fe507416a3fd8e6

--- test/tools/diagnose/cli_sdk/sdk_diagnose.cpp
+++ test/tools/diagnose/cli_sdk/sdk_diagnose.cpp
@@ -424,6 +424,13 @@ static void HandleShow(std::vector<std::string> cmds)
         CLI_PrintBuf("Local Node:");
         CmNodeId localNode = BioClient::Instance()->GetMirror()->GetLocalNodeInfo();
         CLI_PrintBuf("%s\n", localNode.ToString().c_str());
+    } else if (viewType == "trace") {
+        if (cmds.size() != 2) {
+            CLI_PrintBuf("Input parameters failed!, num:%u.\n", cmds.size());
+            return;
+        }
+        auto info = ock::htracer::GetTraceInfo();
+        CLI_PrintBuf(info.c_str());
     }
 }
 
@@ -616,7 +623,7 @@ static void BioSdkDebugHelp(char *command, int detail) noexcept
     CLI_PrintBuf("\tlist all object: sdk listall [prefix]\n");
     CLI_PrintBuf("\tload object: sdk load [key] [offset] [length] [location]\n");
     CLI_PrintBuf("\tdelete object: sdk delete [key] [location]\n");
-    CLI_PrintBuf("\tshow view: sdk show [pt/node] [all/affinity]\n");
+    CLI_PrintBuf("\tshow view: sdk show [pt/node/trace] [all/affinity]\n");
     CLI_PrintBuf("\tperf test: sdk perf [rw] [bs(Kb)] [ioDepth] [size(Mb)]\n");
     CLI_PrintBuf("\texit: exit console\n");
 }
