1d48f869cbf3cf6e8b36046a1b64bdf31bab0068
ljj929 <499109299@qq.com>
2024-04-25 20:35:03 +0800

    【修改描述】增加net模块分支覆盖 【DTS/CR/AR单号】 AR20240127801698 【合入版本】BeiMing 24.2.T1.B001
    Match-id-1d7611bc4bda360558b8e9227c55d642e11ae29d

--- test/tools/bio_tp/bio_server_tp.cpp
+++ test/tools/bio_tp/bio_server_tp.cpp
@@ -12,6 +12,9 @@ void tp::ServerTp::Register() noexcept
     LVOS_TP_REG(SERVER_UNDERFS_GET, "server underfs get error", tp::CommonTp::IntValueCallback);
     LVOS_TP_REG(SERVER_UNDERFS_DELETE, "server underfs delete error", tp::CommonTp::IntValueCallback);
     LVOS_TP_REG(SERVER_UNDERFS_STAT, "server underfs stat error", tp::CommonTp::IntValueCallback);
+    LVOS_TP_REG(SERVER_NET_FAIL_TO_CONNECT_CTRL_PLANE, "Failed to connect ctrl plane to peer target node id", tp::CommonTp::IntValueCallback);
+    LVOS_TP_REG(SERVER_NET_GET_CHANNEL_BY_ID_FAILED, "Failed to get channel by dstNid", tp::CommonTp::IntValueCallback);
+    LVOS_TP_REG(SERVER_NET_PEER_CONNECTION_REFUSED, "peer connection has been refused", tp::CommonTp::IntValueCallback);
 }
 
 void tp::ServerTp::Deregister() noexcept
@@ -20,6 +23,9 @@ void tp::ServerTp::Deregister() noexcept
     LVOS_TP_UNREG(SERVER_UNDERFS_GET);
     LVOS_TP_UNREG(SERVER_UNDERFS_DELETE);
     LVOS_TP_UNREG(SERVER_UNDERFS_STAT);
+    LVOS_TP_UNREG(SERVER_NET_FAIL_TO_CONNECT_CTRL_PLANE);
+    LVOS_TP_UNREG(SERVER_NET_GET_CHANNEL_BY_ID_FAILED);
+    LVOS_TP_UNREG(SERVER_NET_PEER_CONNECTION_REFUSED);
 }
 #else
 void tp::ServerTp::Register() noexcept {}
