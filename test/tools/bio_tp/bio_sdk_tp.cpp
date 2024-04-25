42f0a2fcdb87c71618a402640aae43d6e0d119e3
ljj929 <499109299@qq.com>
2024-04-25 15:30:16 +0800

    【修改描述】回退部分错误DT 【DTS/CR/AR单号】 AR20240127801698 【合入版本】BeiMing 24.2.T1.B001
    Match-id-de47ac4cd1902d74c1c57f834815f4732c2ca834

--- test/tools/bio_tp/bio_sdk_tp.cpp
+++ test/tools/bio_tp/bio_sdk_tp.cpp
@@ -8,22 +8,10 @@ using namespace ock::bio;
 static uint32_t MY_PID = 102;
 void tp::SdkTp::Register() noexcept
 {
-    LVOS_TP_REG(SDK_SET_RET_FAIL, "set sdk ret failed, return BIO_ERR", tp::CommonTp::IntValueCallback);
-    LVOS_TP_REG(SDK_SET_RET_FAIL_CONFLICT, "set sdk ret failed, return BIO_ERR", tp::CommonTp::IntValueCallback);
-    LVOS_TP_REG(SDK_SET_BIO_CLIENT_NET_PRE_INIT_FAIL, "set sdk BioClientNetPreInit ret failed, return BIO_ERR", tp::CommonTp::IntValueCallback);
-    LVOS_TP_REG(SDK_SET_BIO_CLIENT_NET_PRE_INIT_FAIL_CONFLICT, "set sdk BioClientNetPreInit ret failed, return BIO_ERR", tp::CommonTp::IntValueCallback);
-    LVOS_TP_REG(SDK_SET_BIO_CLIENT_MIRROR_INIT_FAIL, "set sdk BioClientMirrorInit ret failed, return BIO_ERR", tp::CommonTp::IntValueCallback);
-    LVOS_TP_REG(SDK_SET_BIO_CLIENT_MIRROR_INIT_FAIL_CONFLICT, "set sdk BioClientMirrorInit ret failed, return BIO_ERR", tp::CommonTp::IntValueCallback);
 }
 
 void tp::SdkTp::Deregister() noexcept
 {
-    LVOS_TP_UNREG(SDK_SET_RET_FAIL);
-    LVOS_TP_UNREG(SDK_SET_RET_FAIL_CONFLICT);
-    LVOS_TP_UNREG(SDK_SET_BIO_CLIENT_NET_PRE_INIT_FAIL);
-    LVOS_TP_UNREG(SDK_SET_BIO_CLIENT_NET_PRE_INIT_FAIL_CONFLICT);
-    LVOS_TP_UNREG(SDK_SET_BIO_CLIENT_MIRROR_INIT_FAIL);
-    LVOS_TP_UNREG(SDK_SET_BIO_CLIENT_MIRROR_INIT_FAIL_CONFLICT);
 }
 #else
 void tp::SdkTp::Register() noexcept {}
