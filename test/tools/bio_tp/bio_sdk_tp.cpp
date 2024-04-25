66607266e7908d3adfddb81628165a81958c21b1
ljj929 <499109299@qq.com>
2024-04-25 21:15:12 +0800

    【修改描述】提升SDK模块DT分支覆盖率 【DTS/CR/AR单号】 AR20240127801698 【合入版本】BeiMing 24.2.T1.B001
    Match-id-1100bc913b308466eaf5eb1fb3bcc2ff0e97ceac

--- test/tools/bio_tp/bio_sdk_tp.cpp
+++ test/tools/bio_tp/bio_sdk_tp.cpp
@@ -8,10 +8,19 @@ using namespace ock::bio;
 static uint32_t MY_PID = 102;
 void tp::SdkTp::Register() noexcept
 {
+    LVOS_TP_REG(SDK_MIRROR_PT_VIEW_FIND_FAIL, "sdk mirror mPtView find failed, rturn BIO_INVALID_PARAM", tp::CommonTp::IntValueCallback);
+    LVOS_TP_REG(SDK_MIRROR_CHECK_PT_FAIL, "sdk mirror check pt failed, rturn BIO_CHECK_PT_FAIL", tp::CommonTp::IntValueCallback);
+    LVOS_TP_REG(SDK_MIRROR_PUT_MEMORY_FAIL, "sdk mirror Alloc put memory failed, rturn BIO_INNER_ERR", tp::CommonTp::IntValueCallback);
+    LVOS_TP_REG(SDK_MIRROR_SEND_PUT_FAIL, "sdk mirror send put request failed, rturn BIO_INNER_ERR", tp::CommonTp::IntValueCallback);
+
 }
 
 void tp::SdkTp::Deregister() noexcept
 {
+    LVOS_TP_UNREG(SDK_MIRROR_PT_VIEW_FIND_FAIL);
+    LVOS_TP_UNREG(SDK_MIRROR_CHECK_PT_FAIL);
+    LVOS_TP_UNREG(SDK_MIRROR_PUT_MEMORY_FAIL);
+    LVOS_TP_UNREG(SDK_MIRROR_SEND_PUT_FAIL);
 }
 #else
 void tp::SdkTp::Register() noexcept {}
