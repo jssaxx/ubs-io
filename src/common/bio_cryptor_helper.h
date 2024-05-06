38cf66b72324f5aabc5bce724a22f6198bc6a1d3
ljj929 <499109299@qq.com>
2024-05-06 11:16:17 +0800

    【修改描述】引入hseceasy 【DTS/CR/AR单号】 AR20240120620106 【合入版本】BeiMing 24.2.RC3.B013
    Match-id-cdce786d5ac89719600c9bfa7682ab2ef564f316

---
+++ src/common/bio_cryptor_helper.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
+ */
+
+#ifndef BIO_HSECRYPTORHELPER_H
+#define BIO_HSECRYPTORHELPER_H
+#include <sys/types.h>
+
+#include <cstdint>
+#include <string>
+#include <mutex>
+
+#include "hse_cryptor.h"
+
+namespace ock {
+namespace bio {
+class BioCryptorHelper {
+public:
+    BioCryptorHelper(std::string kfsMaster, std::string kfsStandby) noexcept;
+
+public:
+    int Decrypt(int domainId, const std::string &filePath, std::pair<char *, int> &result) noexcept;
+    int CheckMasterKeyExpired(int domainId, bool &isRkExpired, uint32_t lead) noexcept;
+    static void EraseDecryptData(std::pair<char *, int> &data) noexcept;
+
+private:
+    class CryptorHelperInitializer {
+    public:
+        CryptorHelperInitializer(const std::string &m, const std::string &s) noexcept;
+        ~CryptorHelperInitializer() noexcept;
+
+    public:
+        bool Initialized() const noexcept;
+
+    private:
+        bool initialized;
+        std::unique_lock<std::mutex> lockGuard;
+    };
+
+private:
+    const std::string kfsMasterPath;
+    const std::string kfsStandbyPath;
+    static std::mutex globalMutex;
+};
+}
+}
+
+#endif // BIO_HSECRYPTORHELPER_H
