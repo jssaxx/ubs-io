42fe22bc46687b57fbc357c6acbd616d64c54ecc
user <user@domain.com>
2024-07-13 14:47:59 +0800

    【修改描述】支持bio独立部署,内存配置为0，disk配置空 【DTS/CR/AR单号】 #332 【合入版本】BeiMing-K 24.0.RC3
    Match-id-12b14511604f594950363e82f0a3ac139d8b1701

--- src/interceptor/client/interceptor_net.cpp
+++ src/interceptor/client/interceptor_net.cpp
@@ -82,6 +82,10 @@ int32_t InterceptorClientNetService::StartNetService()
 
 BResult InterceptorClientNetService::CorrectFd()
 {
+    //分离部署mShmFd=-1，直接返回
+    if(mShmFd == -1){
+        return BIO_OK;
+    }
     int32_t realFd = -1;
     auto result = mNetEngine->ReceiveFds(INVALID_NID, &realFd, 1U);
     if (UNLIKELY(result != BIO_OK)) {
@@ -110,6 +114,11 @@ BResult InterceptorClientNetService::CheckShmFd()
 
 BResult InterceptorClientNetService::ShmInitInner()
 {
+    //mShmFd = -1，分离部署server端未初始化shm，直接返回
+    if (mShmFd == -1) {
+        CLOG_INFO("mShmFd is -1,not need ShmInitInner.");
+        return BIO_OK;
+    }
     if (UNLIKELY(CheckShmFd() != BIO_OK)) {
         mShmFd = -1;
         return BIO_ERR;
@@ -140,7 +149,8 @@ BResult InterceptorClientNetService::ShmInit()
     mShmFd = rsp.memFd;
     mShmOffset = rsp.offset;
     mShmLength = rsp.length;
-    if (UNLIKELY(mShmOffset != 0 || mShmLength > defaultMaxShmSize)) {
+    //return ok 且 mShmFd=-1,属于分离部署server端未初始化shm
+    if (mShmFd != -1 && (UNLIKELY(mShmOffset != 0 || mShmLength > defaultMaxShmSize))) {
         CLOG_ERROR("Get share memory offset:" << mShmOffset << ", length:" << mShmLength << " wrong.");
         return BIO_ERR;
     }
