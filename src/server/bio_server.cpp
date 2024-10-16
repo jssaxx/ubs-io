66ac21287ddb77109e8d264323ea32dfbacf016a
user <user@domain.com>
2024-10-16 14:59:36 +0800

    【修改描述】dlopen使用绝对路径，避免加载任意so 【DTS/CR/AR单号】DTS2024101549183 【合入版本】BeiMing 24.2.0.B001
    Match-id-40d9bac5998c8dc65aea323de0005a9c6cb72611

--- src/server/bio_server.cpp
+++ src/server/bio_server.cpp
@@ -512,10 +512,24 @@ BResult BioServer::BioServerDiagnoseInit()
 using ServerDiagnose = int (*)();
 BResult BioServer::BioServerDiagnoseInitInner()
 {
-    const char *soFileName = "libserver_diagnose.so";
     void *handler = nullptr;
+#ifdef DEBUG_UT
+    const char *soFileName = "libserver_diagnose.so";
     LVOS_TP_START(CLI_SERVER_DIAGNOSE_HANDLER_ERR, &handler, nullptr);
     handler = dlopen(soFileName, RTLD_NOW);
+#else
+    std::string soFileName = std::string(PROJECT_PATH_PREFIX) + "/lib/libserver_diagnose.so";
+    char *canonicalPath = realpath(soFileName.c_str(), nullptr);
+    if (canonicalPath == nullptr) {
+        LOG_ERROR("Failed to open library, not exist, " << soFileName << ".");
+        return BIO_NOT_EXISTS;
+    }
+
+    LVOS_TP_START(CLI_SERVER_DIAGNOSE_HANDLER_ERR, &handler, nullptr);
+    handler = dlopen(canonicalPath, RTLD_NOW);
+    free(canonicalPath);
+    canonicalPath = nullptr;
+#endif
     LVOS_TP_END;
     if (handler == nullptr) {
         LOG_ERROR("Failed to open library() " << soFileName << " dlopen , error " << dlerror());
