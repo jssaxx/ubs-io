11e7703687ffe4a377d819ce366d8c5c4f413535
ljj929 <499109299@qq.com>
2024-11-16 14:58:34 +0800

    【修改描述】 增加ut 【DTS/CR/AR单号】 #391 【合入版本】BeiMing 24.2.0
    Match-id-b4cd2fa49e7b7e3aa59622af7fbf6b5ef9d58f44

--- test/llt/unit-tests/cm/test_cm.cpp
+++ test/llt/unit-tests/cm/test_cm.cpp
@@ -674,3 +674,10 @@ TEST_F(TestCm, test_cm_get_zk_node_state)
     auto ret = CmClientZkGetNodeState(0, 0, &cmState);
     EXPECT_EQ(ret, CM_ERR);
 }
+
+TEST_F(TestCm, test_cm_view_create_store)
+{
+LOG_INFO("test_cm_view_create_store");
+auto ret = CmClientZkGetNodeState(3, UINT32_MAX,2);
+EXPECT_EQ(ret, NULL);
+}
