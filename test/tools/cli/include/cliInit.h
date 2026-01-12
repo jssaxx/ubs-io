fa2105d3ca30fe6f3014d506f877648ef343eb08
user <user@domain.com>
2026-01-12 11:19:32 +0800

    删除tools benchmarks目录
    Match-id-4c7207703462d3808e26ec40954fe6726cecd3b8

--- test/tools/cli/include/cliInit.h
+++
@@ -1,9 +0,0 @@
-/*
-* Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
-* Author: j00792703
-*/
-
-#pragma once
-
-int DiagnoseClientInit();
-void DiagnoseClientExit();
--- test/tools/cli/src/cliInit.cpp
+++
@@ -1,8 +0,0 @@
-
-/*
-* Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
-* Author: j00792703
-*/
-
-#pragma once
-#include "cliInit.h"
