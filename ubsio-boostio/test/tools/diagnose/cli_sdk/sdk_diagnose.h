a09e9d21524a8ac0dd676971d32b607b4f784fd9
ljj929 <499109299@qq.com>
2026-02-03 16:58:49 +0800

    diagnose dlopen

--- ubsio-boostio/test/tools/diagnose/cli_sdk/sdk_diagnose.h
+++ ubsio-boostio/test/tools/diagnose/cli_sdk/sdk_diagnose.h
@@ -13,16 +13,50 @@
 #ifndef BOOSTIO_SDK_DIAGNOSE_H
 #define BOOSTIO_SDK_DIAGNOSE_H
 
+#include "cli_define.h"
+
 namespace ock {
-    namespace bio {
-        namespace diagnose {
-            class BioSdkCommand {
-            public:
-                static int Initialize() noexcept;
-                static void Destroy() noexcept;
-            };
-        }
-    }
+namespace bio {
+namespace diagnose {
+class BioSdkCommand {
+public:
+    static int Initialize() noexcept;
+    static void Destroy() noexcept;
+    static int32_t LoadSymbols();
+
+    static void BioSdkDebugProcess(int argc, char *argv[]) noexcept;
+    static void BioSdkDebugHelp(char *command, int detail) noexcept;
+    static void HandleListCache();
+    static void HandleCreate(const std::vector<std::string> &cmds);
+    static void HandleOpen(const std::vector<std::string> &cmds);
+    static void HandleDestroy(const std::vector<std::string> &cmds);
+    static void HandlePut(const std::vector<std::string> &cmds);
+    static void HandleGet(const std::vector<std::string> &cmds);
+    static void HandleList(const std::vector<std::string> &cmds);
+    static void HandleStat(const std::vector<std::string> &cmds);
+    static void HandleLoad(const std::vector<std::string> &cmds);
+    static void HandleDelete(const std::vector<std::string> &cmds);
+    static void HandleAddDisk(const std::vector<std::string> &cmds);
+    static void HandleShow(const std::vector<std::string> &cmds);
+    static void HandleShowCacheHit(const std::vector<std::string> &cmds);
+    static void HandleShowCacheResource(const std::vector<std::string> &cmds);
+    static void HandleNotifyUpdatePrepare(const std::vector<std::string> &cmds);
+    static void HandleNotifyUpdateFinish(const std::vector<std::string> &cmds);
+    static void HandleCheckUpdateReady(const std::vector<std::string> &cmds);
+    static void HandleSdkTrace(const std::vector<std::string> &cmds);
+    static void* PerfTestPutImpl(void *param);
+    static void* PerfTestGetImpl(void *param);
+    static void HandlePerf(const std::vector<std::string> &cmds);
+
+private:
+    static bool mInited;
+    static void *mHandler;
+    static CliRegCmdFuncPtr mRegOp;
+    static CliUnRegCmdFuncPtr mUnRegOp;
+    static CliPrintBufFuncPtr mPrintOp;
+};
+}
+}
 }
 
 #ifdef __cplusplus
@@ -35,4 +69,4 @@ extern int SdkDiagnoseInit();
 }
 #endif
 
-#endif //BOOSTIO_SDK_DIAGNOSE_H
+#endif // BOOSTIO_SDK_DIAGNOSE_H
--- ubsio-boostio/test/tools/diagnose/cli_server/server_diagnose.h
+++ ubsio-boostio/test/tools/diagnose/cli_server/server_diagnose.h
@@ -13,16 +13,40 @@
 #ifndef BOOSTIO_SERVER_DIAGNOSE_H
 #define BOOSTIO_SERVER_DIAGNOSE_H
 
+#include "cli_define.h"
+
 namespace ock {
-    namespace bio {
-        namespace diagnose {
-            class BioServerCommand {
-            public:
-                static int Initialize() noexcept;
-                static void Destroy() noexcept;
-            };
-        }
-    }
+namespace bio {
+namespace diagnose {
+
+class BioServerCommand {
+public:
+    static int Initialize() noexcept;
+    static void Destroy() noexcept;
+    static int32_t LoadSymbols();
+
+    static void BioServerDebugProcess(int argc, char *argv[]) noexcept;
+    static void BioServerDebugHelp(char *command, int detail) noexcept;
+    static void HandleModifyEvictWaterLevel(uint8_t tier, uint64_t level);
+    static void HandleModifyEvictMemQuantity(uint64_t quantity);
+    static void HandleModifyEvictDiskQuantity(uint64_t quantity);
+    static void HandleModifyMemReadWriteRatio(const std::string &ratios);
+    static void HandleModifyDiskReadWriteRatio(const std::string &ratios);
+    static void BioServerHandleShow(const std::vector<std::string> &cmds);
+    static void HandleServerTrace(const std::vector<std::string> &cmds);
+    static void HandleRCachePut(const std::vector<std::string> &cmds);
+    static void HandleRCacheGet(const std::vector<std::string> &cmds);
+    static void HandleRCacheDelete(const std::vector<std::string> &cmds);
+
+private:
+    static bool mInited;
+    static void *mHandler;
+    static CliRegCmdFuncPtr mRegOp;
+    static CliUnRegCmdFuncPtr mUnRegOp;
+    static CliPrintBufFuncPtr mPrintOp;
+};
+}
+}
 }
 
 #ifdef __cplusplus
