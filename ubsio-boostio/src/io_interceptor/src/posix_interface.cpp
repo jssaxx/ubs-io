69c1d10fdeb616b2d9a88d7736eea0d6606329ee
ljj929 <499109299@qq.com>
2026-04-23 10:06:51 +0800

    refine interceptor hook logs

--- ubsio-boostio/src/io_interceptor/src/posix_interface.cpp
+++ ubsio-boostio/src/io_interceptor/src/posix_interface.cpp
@@ -22,289 +22,378 @@
 
 using namespace ock::interceptor;
 
+#define POSIX_HOOK_ENTER(args...) INTERCEPTORLOG_DEBUG("Intercept %s enter, " args, __FUNCTION__)
+#define POSIX_HOOK_SUCCESS(args...) INTERCEPTORLOG_DEBUG("Intercept %s success, " args, __FUNCTION__)
+
 extern "C" {
 INTERCEPTOR_API int open(const char* path, int flags, ...)
 {
-    INTERCEPTORLOG_DEBUG("Hooking open %s  %d succeeded.", path, flags);
+    POSIX_HOOK_ENTER("path:%s, flags:%d.", path, flags);
     va_list args;
     va_start(args, flags);
     auto ret = HookOpen(path, flags, args);
     va_end(args);
+    POSIX_HOOK_SUCCESS("path:%s, flags:%d, ret:%d.", path, flags, ret);
     return ret;
 }
 
 INTERCEPTOR_API int open64(const char* path, int flags, ...)
 {
-    INTERCEPTORLOG_DEBUG("Hooking open64 %s %d succeeded.", path, flags);
+    POSIX_HOOK_ENTER("path:%s, flags:%d.", path, flags);
     va_list args;
     va_start(args, flags);
     auto ret = HookOpen64(path, flags, args);
     va_end(args);
+    POSIX_HOOK_SUCCESS("path:%s, flags:%d, ret:%d.", path, flags, ret);
     return ret;
 }
 
 INTERCEPTOR_API int openat(int dirfd, const char *path, int flags, ...)
 {
-    INTERCEPTORLOG_DEBUG("Hooking openat %d %s succeeded.", dirfd, path);
+    POSIX_HOOK_ENTER("dirfd:%d, path:%s, flags:%d.", dirfd, path, flags);
     va_list args;
     va_start(args, flags);
     auto ret = HookOpenAt(dirfd, path, flags, args);
     va_end(args);
+    POSIX_HOOK_SUCCESS("dirfd:%d, path:%s, flags:%d, ret:%d.", dirfd, path, flags, ret);
     return ret;
 }
 
 INTERCEPTOR_API int openat64(int dirfd, const char *path, int flags, ...)
 {
-    INTERCEPTORLOG_DEBUG("Hooking openat64 %d %s succeeded.", dirfd, path);
+    POSIX_HOOK_ENTER("dirfd:%d, path:%s, flags:%d.", dirfd, path, flags);
     va_list args;
     va_start(args, flags);
     auto ret = HookOpenAt64(dirfd, path, flags, args);
     va_end(args);
+    POSIX_HOOK_SUCCESS("dirfd:%d, path:%s, flags:%d, ret:%d.", dirfd, path, flags, ret);
     return ret;
 }
 
 INTERCEPTOR_API int creat(const char* path, mode_t mode)
 {
-    INTERCEPTORLOG_DEBUG("Hooking creat %s %o succeeded.", path, mode);
-    return HookCreat(path, mode);
+    POSIX_HOOK_ENTER("path:%s, mode:%o.", path, mode);
+    auto ret = HookCreat(path, mode);
+    POSIX_HOOK_SUCCESS("path:%s, mode:%o, ret:%d.", path, mode, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int creat64(const char* path, mode_t mode)
 {
-    INTERCEPTORLOG_DEBUG("Hooking creat64 %s %o succeeded.", path, mode);
-    return HookCreat64(path, mode);
+    POSIX_HOOK_ENTER("path:%s, mode:%o.", path, mode);
+    auto ret = HookCreat64(path, mode);
+    POSIX_HOOK_SUCCESS("path:%s, mode:%o, ret:%d.", path, mode, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int close(int fd)
 {
-    INTERCEPTORLOG_DEBUG("Hooking close %d succeeded.", fd);
-    return HookClose(fd);
+    POSIX_HOOK_ENTER("fd:%d.", fd);
+    auto ret = HookClose(fd);
+    POSIX_HOOK_SUCCESS("fd:%d, ret:%d.", fd, ret);
+    return ret;
 }
 
 INTERCEPTOR_API off_t lseek(int fd, __off_t offset, int whence)
 {
-    INTERCEPTORLOG_DEBUG("Hooking lseek %d %lld %d succeeded.", fd, offset, whence);
-    return HookLseek(fd, offset, whence);
+    POSIX_HOOK_ENTER("fd:%d, offset:%lld, whence:%d.", fd, offset, whence);
+    auto ret = HookLseek(fd, offset, whence);
+    POSIX_HOOK_SUCCESS("fd:%d, offset:%lld, whence:%d, ret:%lld.", fd, offset, whence, ret);
+    return ret;
 }
 
 INTERCEPTOR_API off64_t lseek64(int fd, __off64_t offset, int whence)
 {
-    INTERCEPTORLOG_DEBUG("Hooking lseek64 %d %lld %d succeeded.", fd, offset, whence);
-    return HookLseek64(fd, offset, whence);
+    POSIX_HOOK_ENTER("fd:%d, offset:%lld, whence:%d.", fd, offset, whence);
+    auto ret = HookLseek64(fd, offset, whence);
+    POSIX_HOOK_SUCCESS("fd:%d, offset:%lld, whence:%d, ret:%lld.", fd, offset, whence, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t read(int fd, void* buf, size_t nbytes)
 {
-    INTERCEPTORLOG_DEBUG("Hooking read %d %llu succeeded.", fd, nbytes);
-    return HookRead(fd, buf, nbytes);
+    POSIX_HOOK_ENTER("fd:%d, nbytes:%llu.", fd, nbytes);
+    auto ret = HookRead(fd, buf, nbytes);
+    POSIX_HOOK_SUCCESS("fd:%d, nbytes:%llu, ret:%zd.", fd, nbytes, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t pread(int fd, void* buf, size_t nbyte, off_t offset)
 {
-    INTERCEPTORLOG_DEBUG("Hooking pread %d %llu %lld succeeded.", fd, nbyte, offset);
-    return HookPread(fd, buf, nbyte, offset);
+    POSIX_HOOK_ENTER("fd:%d, nbytes:%llu, offset:%lld.", fd, nbyte, offset);
+    auto ret = HookPread(fd, buf, nbyte, offset);
+    POSIX_HOOK_SUCCESS("fd:%d, nbytes:%llu, offset:%lld, ret:%zd.", fd, nbyte, offset, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t pread64(int fildes, void *buf, size_t nbyte, off64_t offset)
 {
-    return HookPread64(fildes, buf, nbyte, offset);
+    POSIX_HOOK_ENTER("fd:%d, nbytes:%llu, offset:%lld.", fildes, nbyte, offset);
+    auto ret = HookPread64(fildes, buf, nbyte, offset);
+    POSIX_HOOK_SUCCESS("fd:%d, nbytes:%llu, offset:%lld, ret:%zd.", fildes, nbyte, offset, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t readv(int fd, const struct iovec* vector, int count)
 {
-    INTERCEPTORLOG_DEBUG("Hooking readv %d %d succeeded.", fd, count);
-    return HookReadv(fd, vector, count);
+    POSIX_HOOK_ENTER("fd:%d, iovcnt:%d.", fd, count);
+    auto ret = HookReadv(fd, vector, count);
+    POSIX_HOOK_SUCCESS("fd:%d, iovcnt:%d, ret:%zd.", fd, count, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t preadv(int fd, const struct iovec* vector, int iovcnt, off_t offset)
 {
-    INTERCEPTORLOG_DEBUG("Hooking preadv %d %d %lld succeeded.", fd, iovcnt, offset);
-    return HookPreadv(fd, vector, iovcnt, offset);
+    POSIX_HOOK_ENTER("fd:%d, iovcnt:%d, offset:%lld.", fd, iovcnt, offset);
+    auto ret = HookPreadv(fd, vector, iovcnt, offset);
+    POSIX_HOOK_SUCCESS("fd:%d, iovcnt:%d, offset:%lld, ret:%zd.", fd, iovcnt, offset, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t preadv64(int fd, const struct iovec* vector, int iovcnt, off64_t offset)
 {
-    INTERCEPTORLOG_DEBUG("Hooking preadv64 %d %d %lld succeeded.", fd, iovcnt, offset);
-    return HookPreadv64(fd, vector, iovcnt, offset);
+    POSIX_HOOK_ENTER("fd:%d, iovcnt:%d, offset:%lld.", fd, iovcnt, offset);
+    auto ret = HookPreadv64(fd, vector, iovcnt, offset);
+    POSIX_HOOK_SUCCESS("fd:%d, iovcnt:%d, offset:%lld, ret:%zd.", fd, iovcnt, offset, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t write(int fd, const void* buf, size_t nbytes)
 {
-    INTERCEPTORLOG_DEBUG("Hooking write %d %llu succeeded.", fd, nbytes);
-    return HookWrite(fd, buf, nbytes);
+    POSIX_HOOK_ENTER("fd:%d, nbytes:%llu.", fd, nbytes);
+    auto ret = HookWrite(fd, buf, nbytes);
+    POSIX_HOOK_SUCCESS("fd:%d, nbytes:%llu, ret:%zd.", fd, nbytes, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t pwrite(int fd, const void* buf, size_t count, off_t offset)
 {
-    INTERCEPTORLOG_DEBUG("Hooking pwrite %d %llu %lld succeeded.", fd, count, offset);
-    return HookPwrite(fd, buf, count, offset);
+    POSIX_HOOK_ENTER("fd:%d, nbytes:%llu, offset:%lld.", fd, count, offset);
+    auto ret = HookPwrite(fd, buf, count, offset);
+    POSIX_HOOK_SUCCESS("fd:%d, nbytes:%llu, offset:%lld, ret:%zd.", fd, count, offset, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t pwrite64(int fd, const void* buf, size_t count, off_t offset)
 {
-    INTERCEPTORLOG_DEBUG("Hooking pwrite64 %d %llu %lld succeeded.", fd, count, offset);
-    return HookPwrite64(fd, buf, count, offset);
+    POSIX_HOOK_ENTER("fd:%d, nbytes:%llu, offset:%lld.", fd, count, offset);
+    auto ret = HookPwrite64(fd, buf, count, offset);
+    POSIX_HOOK_SUCCESS("fd:%d, nbytes:%llu, offset:%lld, ret:%zd.", fd, count, offset, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t writev(int fd, const struct iovec* vector, int count)
 {
-    INTERCEPTORLOG_DEBUG("Hooking writev %d %d succeeded.", fd, count);
-    return HookWritev(fd, vector, count);
+    POSIX_HOOK_ENTER("fd:%d, iovcnt:%d.", fd, count);
+    auto ret = HookWritev(fd, vector, count);
+    POSIX_HOOK_SUCCESS("fd:%d, iovcnt:%d, ret:%zd.", fd, count, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t pwritev(int fd, const struct iovec* vector, int iovcnt, off_t offset)
 {
-    INTERCEPTORLOG_DEBUG("Hooking pwritev %d %d %lld succeeded.", fd, iovcnt, offset);
-    return HookPwritev(fd, vector, iovcnt, offset);
+    POSIX_HOOK_ENTER("fd:%d, iovcnt:%d, offset:%lld.", fd, iovcnt, offset);
+    auto ret = HookPwritev(fd, vector, iovcnt, offset);
+    POSIX_HOOK_SUCCESS("fd:%d, iovcnt:%d, offset:%lld, ret:%zd.", fd, iovcnt, offset, ret);
+    return ret;
 }
 
 INTERCEPTOR_API ssize_t pwritev64(int fd, const struct iovec* vector, int iovcnt, off64_t offset)
 {
-    INTERCEPTORLOG_DEBUG("Hooking pwritev64 %d %d %lld succeeded.", fd, iovcnt, offset);
-    return HookPwritev64(fd, vector, iovcnt, offset);
+    POSIX_HOOK_ENTER("fd:%d, iovcnt:%d, offset:%lld.", fd, iovcnt, offset);
+    auto ret = HookPwritev64(fd, vector, iovcnt, offset);
+    POSIX_HOOK_SUCCESS("fd:%d, iovcnt:%d, offset:%lld, ret:%zd.", fd, iovcnt, offset, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int dup(int fd)
 {
-    INTERCEPTORLOG_DEBUG("Hooking dup %d succeeded.", fd);
-    return HookDup(fd);
+    POSIX_HOOK_ENTER("fd:%d.", fd);
+    auto ret = HookDup(fd);
+    POSIX_HOOK_SUCCESS("fd:%d, ret:%d.", fd, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int dup2(int fd, int fd2)
 {
-    INTERCEPTORLOG_DEBUG("Hooking dup2 %d %d succeeded.", fd, fd2);
-    return HookDup2(fd, fd2);
+    POSIX_HOOK_ENTER("oldfd:%d, newfd:%d.", fd, fd2);
+    auto ret = HookDup2(fd, fd2);
+    POSIX_HOOK_SUCCESS("oldfd:%d, newfd:%d, ret:%d.", fd, fd2, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int dup3(int fd, int fd2, int flags)
 {
-    INTERCEPTORLOG_DEBUG("Hooking dup3 %d %d succeeded.", fd, fd2);
-    return HookDup3(fd, fd2, flags);
+    POSIX_HOOK_ENTER("oldfd:%d, newfd:%d, flags:%d.", fd, fd2, flags);
+    auto ret = HookDup3(fd, fd2, flags);
+    POSIX_HOOK_SUCCESS("oldfd:%d, newfd:%d, flags:%d, ret:%d.", fd, fd2, flags, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int __xstat(int ver, const char* path, struct stat* stat_buf)
 {
-    INTERCEPTORLOG_DEBUG("Hooking stat %d %s succeeded.", ver, path);
-    return HookStat(ver, path, stat_buf);
+    POSIX_HOOK_ENTER("ver:%d, path:%s.", ver, path);
+    auto ret = HookStat(ver, path, stat_buf);
+    POSIX_HOOK_SUCCESS("ver:%d, path:%s, ret:%d.", ver, path, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int __xstat64(int ver, const char* path, struct stat64* stat_buf)
 {
-    INTERCEPTORLOG_DEBUG("Hooking stat64 %d %s succeeded.", ver, path);
-    return HookStat64(ver, path, stat_buf);
+    POSIX_HOOK_ENTER("ver:%d, path:%s.", ver, path);
+    auto ret = HookStat64(ver, path, stat_buf);
+    POSIX_HOOK_SUCCESS("ver:%d, path:%s, ret:%d.", ver, path, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int __lxstat(int ver, const char* path, struct stat* stat_buf)
 {
-    INTERCEPTORLOG_DEBUG("Hooking lstat %d %s succeeded.", ver, path);
-    return HookLstat(ver, path, stat_buf);
+    POSIX_HOOK_ENTER("ver:%d, path:%s.", ver, path);
+    auto ret = HookLstat(ver, path, stat_buf);
+    POSIX_HOOK_SUCCESS("ver:%d, path:%s, ret:%d.", ver, path, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int __lxstat64(int ver, const char* path, struct stat64* stat_buf)
 {
-    INTERCEPTORLOG_DEBUG("Hooking lstat64 %d %s succeeded.", ver, path);
-    return HookLstat64(ver, path, stat_buf);
+    POSIX_HOOK_ENTER("ver:%d, path:%s.", ver, path);
+    auto ret = HookLstat64(ver, path, stat_buf);
+    POSIX_HOOK_SUCCESS("ver:%d, path:%s, ret:%d.", ver, path, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int __fxstat(int ver, int fd, struct stat* buf)
 {
-    INTERCEPTORLOG_DEBUG("Hooking fstat %d %d succeeded.", ver, fd);
-    return HookFstat(ver, fd, buf);
+    POSIX_HOOK_ENTER("ver:%d, fd:%d.", ver, fd);
+    auto ret = HookFstat(ver, fd, buf);
+    POSIX_HOOK_SUCCESS("ver:%d, fd:%d, ret:%d.", ver, fd, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int __fxstat64(int ver, int fd, struct stat64* buf)
 {
-    INTERCEPTORLOG_DEBUG("Hooking fstat64 %d %d succeeded.", ver, fd);
-    return HookFstat64(ver, fd, buf);
+    POSIX_HOOK_ENTER("ver:%d, fd:%d.", ver, fd);
+    auto ret = HookFstat64(ver, fd, buf);
+    POSIX_HOOK_SUCCESS("ver:%d, fd:%d, ret:%d.", ver, fd, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int __fxstatat(int ver, int fd, const char* file, struct stat* buf, int flags)
 {
-    INTERCEPTORLOG_DEBUG("Hooking fstatat %d %d %s %d succeeded.", ver, fd, file, flags);
-    return HookFstatAt(ver, fd, file, buf, flags);
+    POSIX_HOOK_ENTER("ver:%d, dirfd:%d, path:%s, flags:%d.", ver, fd, file, flags);
+    auto ret = HookFstatAt(ver, fd, file, buf, flags);
+    POSIX_HOOK_SUCCESS("ver:%d, dirfd:%d, path:%s, flags:%d, ret:%d.", ver, fd, file, flags, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int __fxstatat64(int ver, int fd, const char* file, struct stat64* buf, int flags)
 {
-    INTERCEPTORLOG_DEBUG("Hooking fstatat64 %d %d %s %d succeeded.", ver, fd, file, flags);
-    return HookFstatAt64(ver, fd, file, buf, flags);
+    POSIX_HOOK_ENTER("ver:%d, dirfd:%d, path:%s, flags:%d.", ver, fd, file, flags);
+    auto ret = HookFstatAt64(ver, fd, file, buf, flags);
+    POSIX_HOOK_SUCCESS("ver:%d, dirfd:%d, path:%s, flags:%d, ret:%d.", ver, fd, file, flags, ret);
+    return ret;
 }
 
 int INTERCEPTOR_API access(const char* path, int mode)
 {
-    INTERCEPTORLOG_DEBUG("Hooking access %s %o succeeded.", path, mode);
-    return HookAccess(path, mode);
+    POSIX_HOOK_ENTER("path:%s, mode:%o.", path, mode);
+    auto ret = HookAccess(path, mode);
+    POSIX_HOOK_SUCCESS("path:%s, mode:%o, ret:%d.", path, mode, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int unlink(const char* path)
 {
-    INTERCEPTORLOG_DEBUG("Hooking unlink %s succeeded.", path);
-    return HookUnlink(path);
+    POSIX_HOOK_ENTER("path:%s.", path);
+    auto ret = HookUnlink(path);
+    POSIX_HOOK_SUCCESS("path:%s, ret:%d.", path, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int unlinkat(int fd, const char *pathname, int flag)
 {
-    INTERCEPTORLOG_DEBUG("Hooking unlinkat %d %s %d succeeded.", fd, pathname, flag);
-    return HookUnlinkat(fd, pathname, flag);
+    POSIX_HOOK_ENTER("dirfd:%d, path:%s, flags:%d.", fd, pathname, flag);
+    auto ret = HookUnlinkat(fd, pathname, flag);
+    POSIX_HOOK_SUCCESS("dirfd:%d, path:%s, flags:%d, ret:%d.", fd, pathname, flag, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int fsync(int fd)
 {
-    INTERCEPTORLOG_DEBUG("Hooking fsync %d succeeded.", fd);
-    return HookFsync(fd);
+    POSIX_HOOK_ENTER("fd:%d.", fd);
+    auto ret = HookFsync(fd);
+    POSIX_HOOK_SUCCESS("fd:%d, ret:%d.", fd, ret);
+    return ret;
 }
 
 INTERCEPTOR_API void sync(void)
 {
-    INTERCEPTORLOG_DEBUG("Hooking sync succeeded.");
+    POSIX_HOOK_ENTER("void.");
     HookSync();
+    POSIX_HOOK_SUCCESS("completed.");
 }
 
 INTERCEPTOR_API int syncfs(int fd)
 {
-    INTERCEPTORLOG_DEBUG("Hooking syncfs fd(%d) succeeded.", fd);
-    return HookSyncFs(fd);
+    POSIX_HOOK_ENTER("fd:%d.", fd);
+    auto ret = HookSyncFs(fd);
+    POSIX_HOOK_SUCCESS("fd:%d, ret:%d.", fd, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int truncate(const char* filename, off_t length)
 {
-    INTERCEPTORLOG_DEBUG("Hooking truncate %s %lld succeeded.", filename, length);
-    return HookTruncate(filename, length);
+    POSIX_HOOK_ENTER("path:%s, length:%lld.", filename, length);
+    auto ret = HookTruncate(filename, length);
+    POSIX_HOOK_SUCCESS("path:%s, length:%lld, ret:%d.", filename, length, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int truncate64(const char* filename, off_t length)
 {
-    INTERCEPTORLOG_DEBUG("Hooking truncate64 %s %lld succeeded.", filename, length);
-    return HookTruncate64(filename, length);
+    POSIX_HOOK_ENTER("path:%s, length:%lld.", filename, length);
+    auto ret = HookTruncate64(filename, length);
+    POSIX_HOOK_SUCCESS("path:%s, length:%lld, ret:%d.", filename, length, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int ftruncate(int fd, off_t length)
 {
-    INTERCEPTORLOG_DEBUG("Hooking ftruncate %d %lld succeeded.", fd, length);
-    return HookFtruncate(fd, length);
+    POSIX_HOOK_ENTER("fd:%d, length:%lld.", fd, length);
+    auto ret = HookFtruncate(fd, length);
+    POSIX_HOOK_SUCCESS("fd:%d, length:%lld, ret:%d.", fd, length, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int ftruncate64(int fd, off_t length)
 {
-    INTERCEPTORLOG_DEBUG("Hooking ftruncate64 %d %lld succeeded.", fd, length);
-    return HookFtruncate64(fd, length);
+    POSIX_HOOK_ENTER("fd:%d, length:%lld.", fd, length);
+    auto ret = HookFtruncate64(fd, length);
+    POSIX_HOOK_SUCCESS("fd:%d, length:%lld, ret:%d.", fd, length, ret);
+    return ret;
 }
 
 int INTERCEPTOR_API rename(const char* oldName, const char* newName)
 {
-    INTERCEPTORLOG_DEBUG("Hooking rename %s %s succeeded.", oldName, newName);
-    return HookRename(oldName, newName);
+    POSIX_HOOK_ENTER("old:%s, new:%s.", oldName, newName);
+    auto ret = HookRename(oldName, newName);
+    POSIX_HOOK_SUCCESS("old:%s, new:%s, ret:%d.", oldName, newName, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int utimes(const char *path, const struct timeval times[2])
 {
-    INTERCEPTORLOG_DEBUG("Hooking utimes %s succeeded.", path);
-    return HookUtimes(path, times);
+    POSIX_HOOK_ENTER("path:%s.", path);
+    auto ret = HookUtimes(path, times);
+    POSIX_HOOK_SUCCESS("path:%s, ret:%d.", path, ret);
+    return ret;
 }
 
 INTERCEPTOR_API int remove(const char* path)
 {
-    INTERCEPTORLOG_DEBUG("Hooking remove %s succeeded.", path);
-    return HookRemove(path);
+    POSIX_HOOK_ENTER("path:%s.", path);
+    auto ret = HookRemove(path);
+    POSIX_HOOK_SUCCESS("path:%s, ret:%d.", path, ret);
+    return ret;
 }
 }
