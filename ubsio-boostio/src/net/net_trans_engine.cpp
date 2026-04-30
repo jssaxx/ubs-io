4038c73a6bfdef7dd7534db64c8b7187e3bc0361
henian_Ni <nihenian@huawei.com>
2026-04-30 09:32:04 +0800

    add trans log

--- ubsio-boostio/src/net/net_trans_engine.cpp
+++ ubsio-boostio/src/net/net_trans_engine.cpp
@@ -262,7 +262,7 @@ BResult MfTransEngine::Read(TransParam& param)
         NET_LOG_ERROR("Failed to read from mf trans, ret: " << ret);
         return ret;
     }
-
+    NET_LOG_WARN("Read success, local addr: " << param.localAddrs[0] << ", remote addr: " << param.remoteAddrs[0] << ", size: " << param.dataSizes[0]);
     return BIO_OK;
 }
 
@@ -287,6 +287,7 @@ BResult MfTransEngine::BatchRead(TransParam& param)
         NET_LOG_ERROR("Failed to batch read from mf trans, ret: " << ret);
         return ret;
     }
+    NET_LOG_WARN( "BatchRead success, size: " << param.localAddrs.size() << ", localAddrs: " << param.localAddrs[0]);
     return BIO_OK;
 }
 
@@ -307,6 +308,7 @@ BResult MfTransEngine::Write(TransParam& param)
         NET_LOG_ERROR("Failed to write to mf trans, ret: " << ret);
         return ret;
     }
+    NET_LOG_WARN("Write success, local addr: " << param.localAddrs[0] << ", remote addr: " << param.remoteAddrs[0] << ", size: " << param.dataSizes[0]);
     return BIO_OK;
 }
 
@@ -330,6 +332,7 @@ BResult MfTransEngine::BatchWrite(TransParam& param)
         NET_LOG_ERROR("Failed to batch write to mf trans, ret: " << ret);
         return ret;
     }
+    NET_LOG_WARN("BatchWrite success, size: " << param.localAddrs.size() << ", localAddrs: " << param.localAddrs[0]);
     return BIO_OK;
 }
 
