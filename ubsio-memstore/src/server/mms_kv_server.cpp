6ae9ddcc824e8dc4b6931019e28494bd64602612
yangyao <2533623867@qq.com>
2026-05-07 17:23:57 +0800

    实现mms_kv_server核心方法
    实现融合部署的KV服务器序列号相关方法：
    - GetSeqNoList/MergeSeqNoList/SyncSeqNoData: 序列号同步方法
    - GetSeqNoData/PutSeqNoData: 序列号数据存取
    - HandleGetSeqNoList/HandleGetSeqNoData/HandleUpdatePtVersion: 处理方法
    - NotifyPtMigrate/NotifyServiceable: 通知方法

--- ubsio-memstore/src/server/mms_kv_server.cpp
+++ ubsio-memstore/src/server/mms_kv_server.cpp
@@ -1198,3 +1198,471 @@ BResult MmsKvServer::HandleReplaceRemote(ServiceContext &ctx)
         UNLIKELY(ctx.MessageData() == nullptr)) {
         LOG_ERROR("Receive message len:" << ctx.MessageDataLen() << " or message data invalid.");
         mNetEngine->Reply(ctx, MMS_INVALID_PARAM, nullptr, 0);
+        return MMS_OK;
+    }
+
+    auto ret = ReplaceLocal(ctx.MessageData(), ctx.MessageDataLen());
+    mNetEngine->Reply(ctx, ret, nullptr, 0);
+    return MMS_OK;
+}
+
+BResult MmsKvServer::HandleReplaceRemoteMulti(ServiceContext &ctx)
+{
+    if (UNLIKELY(ctx.MessageDataLen() < sizeof(IoDataRequest)) || UNLIKELY(ctx.MessageDataLen() > mIoCtxBuffLen) ||
+        UNLIKELY(ctx.MessageData() == nullptr)) {
+        LOG_ERROR("Receive message len:" << ctx.MessageDataLen() << " or message data invalid.");
+        mMulticastEngine->Reply(ctx, MMS_INVALID_PARAM, nullptr, 0);
+        return MMS_OK;
+    }
+
+    auto ret = ReplaceLocal(ctx.MessageData(), ctx.MessageDataLen());
+    mMulticastEngine->Reply(ctx, ret, nullptr, 0);
+    return MMS_OK;
+}
+
+void MmsKvServer::ReplaceRemoteMulticast(const std::unordered_set<std::string> &remoteIps, void *ioBuff,
+                                         uint32_t ioLen, Callback &callback)
+{
+    mMulticastEngine->MulticastAsyncCallBuff(remoteIps, ioBuff, ioLen, callback);
+}
+
+void MmsKvServer::ReplaceRemote(uint16_t remoteId[], int32_t remoteNum, void *ioBuff, uint32_t ioLen,
+                                Callback &callback)
+{
+    for (uint16_t i = 0; i < remoteNum; i++) {
+        mNetEngine->AsyncCallBuff(remoteId[i], g_groupIndex, MMS_OP_S_REPLACE, ioBuff, ioLen, callback);
+    }
+}
+
+BResult MmsKvServer::ReplaceLocal(void *ioBuff, uint32_t ioLen)
+{
+    uint32_t itemNum;
+    uint32_t index;
+    IoDataRequest *req = reinterpret_cast<IoDataRequest *>(ioBuff);
+
+    auto ret =
+        mSequence->NegoSeqNo2Slv(req->head.ptId, req->head.groupIndex, req->seqNo, ioBuff, ioLen, req->negoSeqNo);
+    if (UNLIKELY(ret != MMS_OK)) {
+        LOG_ERROR("Nego fail, ret: " << ret << ", ptId:" << req->head.ptId << ", groupIndex:" << req->head.groupIndex
+                                     << ", seq no:" << req->seqNo << ".");
+        return ret;
+    }
+
+    ret = DeCodeReplaceRequest(itemListUpdate, itemNum, reinterpret_cast<uint64_t>(ioBuff), ioLen);
+    if (UNLIKELY(ret != MMS_OK)) {
+        LOG_ERROR("Decode replace request fail, ret:" << ret << ", ptId:" << req->head.ptId);
+        return ret;
+    }
+
+    for (index = 0; index < itemNum; index++) {
+        ret = mCache->Replace({itemListUpdate[index].key, itemListUpdate[index].value, itemListUpdate[index].offset,
+                               itemListUpdate[index].length, static_cast<uint32_t>(itemListUpdate[index].version),
+                               req->head.ptId});
+        if (UNLIKELY(ret != MMS_OK)) {
+            LOG_ERROR("Replace fail, ret:" << ret << ", key:" << itemListUpdate[index].key);
+            break;
+        }
+
+    }
+    return ret;
+}
+
+void MmsKvServer::DeleteRemoteMulticast(const std::unordered_set<std::string> &remoteIps, void *ioBuff, uint32_t ioLen,
+                                        Callback &callback)
+{
+    mMulticastEngine->MulticastAsyncCallBuff(remoteIps, ioBuff, ioLen, callback);
+}
+
+void MmsKvServer::DeleteRemote(uint16_t remoteId[], int32_t remoteNum, void *ioBuff, uint32_t ioLen, Callback &callback)
+{
+    for (uint16_t i = 0; i < remoteNum; i++) {
+        mNetEngine->AsyncCallBuff(remoteId[i], g_groupIndex, MMS_OP_S_DELETE, ioBuff, ioLen, callback);
+    }
+}
+
+BResult MmsKvServer::DeleteLocal(void *ioBuff, uint32_t ioLen)
+{
+    uint32_t itemNum;
+    uint32_t index;
+    IoDataRequest *req = reinterpret_cast<IoDataRequest *>(ioBuff);
+
+    auto ret = mSequence->NegoSeqNo2Slv(req->head.ptId, req->head.groupIndex, req->seqNo, ioBuff, ioLen,
+                                        req->negoSeqNo);
+    if (UNLIKELY(ret != MMS_OK)) {
+        LOG_ERROR("Nego fail, ret: " << ret << ", ptId:" << req->head.ptId << ", groupIndex:" << req->head.groupIndex
+                  << ", seq no:" << req->seqNo << ".");
+        return ret;
+    }
+
+    ret = DeCodeDeleteRequest(itemListDelete, itemNum, reinterpret_cast<uint64_t>(ioBuff), ioLen);
+    if (ret != MMS_OK) {
+        LOG_ERROR("Decode delete request fail, ret:" << ret << ", ptId:" << req->head.ptId);
+        return ret;
+    }
+
+    BResult result = MMS_OK;
+    for (index = 0; index < itemNum; index++) {
+        ret = mCache->Delete(itemListDelete[index].key, itemListDelete[index].version);
+        if (ret != MMS_OK && ret != MMS_KEY_NOT_EXISTS) {
+            LOG_ERROR("Delete cache fail, ret:" << ret << ", key:" << itemListDelete[index].key << ", ptId:" <<
+                      req->head.ptId);
+            result = ret;
+            break;
+        }
+    }
+    return result;
+}
+
+void MmsKvServer::NotifyServiceable(bool serviceable)
+{
+    mCm = MmsServer::Instance()->GetCm();
+    mServiceable.store(serviceable, std::memory_order_release);
+    LOG_INFO("Notify serviceable: " << serviceable << ".");
+}
+
+void MmsKvServer::NotifyPtMigrate(uint16_t ptId)
+{
+    do {
+        LOG_INFO("Notify pt migrate begin, ptId:" << ptId << ".");
+        auto ret = NotifyPtMigrateImpl(ptId);
+        if (ret != MMS_OK) {
+            LOG_ERROR("Notify pt migrate fail:" << ret << ", ptId:" << ptId << ".");
+        }
+        bool isRetry = (ret == MMS_ALLOC_FAIL || ret == MMS_INNER_RETRY ||
+            ret == MMS_NET_RETRY || ret == MMS_CHECK_PT_FAIL);
+        if (!isRetry) {
+            LOG_INFO("Notify pt migrate end, ptId:" << ptId << ", result:" << ret << ".");
+            break;
+        }
+        sleep(NO_2);
+    } while (true);
+    return;
+}
+
+BResult MmsKvServer::NotifyPtMigrateImpl(uint16_t ptId)
+{
+    CmPtInfo ptInfo;
+
+    auto ret = mCm->GetPtInfo(ptId, ptInfo);
+    if (UNLIKELY(ret != MMS_OK)) {
+        LOG_ERROR("Get pt failed, ret: " << ret << ", ptId:" << ptId << ".");
+        return ret;
+    }
+    uint16_t nodeList[MAX_NODES_NUM];
+    uint16_t nodeNum = 0;
+    for (const auto& elem : ptInfo.copys) {
+        if (elem.state != CM_COPY_RUNNING) {
+            continue;
+        }
+        nodeList[nodeNum] = elem.nodeId;
+        nodeNum++;
+    }
+
+    uint16_t groupNum = MmsServer::Instance()->GetConfig()->GetNetConfig().rpcWorkerGroupsNum;
+
+    for (uint16_t groupIndex = 0; groupIndex < groupNum; groupIndex++) {
+        LOG_DEBUG("Nego ptId:" << ptId << ", groupIndex:" << groupIndex << ".");
+        uint64_t seqList[MAX_NODES_NUM][SEQ_QUEUE_LEN] = { {0} };
+        uint16_t locList[MAX_NODES_NUM][SEQ_QUEUE_LEN] = { {0} };
+        uint32_t seqNum[MAX_NODES_NUM] = { 0 };
+        for (uint16_t i = 0; i < nodeNum; i++) {
+            ret = GetSeqNoList(seqList[i], seqNum[i], ptId, ptInfo.version, groupIndex, nodeList[i]);
+            if (UNLIKELY(ret != MMS_OK)) {
+                LOG_ERROR("Get seq list failed, ret: " << ret << ", ptId:" << ptId << ".");
+                return ret;
+            }
+        }
+        uint64_t negoSeqList[SEQ_QUEUE_LEN] = { 0 };
+        uint16_t negoLocList[SEQ_QUEUE_LEN] = { 0 };
+        uint32_t negoNum = 0;
+        for (uint16_t i = 0; i < nodeNum; i++) {
+            ret = MergeSeqNoList(negoSeqList, negoLocList, negoNum, seqList[i], seqNum[i], nodeList[i]);
+            if (UNLIKELY(ret != MMS_OK)) {
+                LOG_ERROR("Merge seq list failed, ret: " << ret << ", ptId:" << ptId << ".");
+                return ret;
+            }
+        }
+        for (uint16_t j = 0; j < negoNum; j++) {
+            for (uint16_t i = 0; i < nodeNum; i++) {
+                ret = SyncSeqNoData(negoSeqList[j], negoLocList[j], seqList[i], seqNum[i], nodeList[i],
+                                    ptInfo, groupIndex);
+                if (UNLIKELY(ret != MMS_OK)) {
+                    LOG_ERROR("Sync seq data failed, ret: " << ret << ", ptId:" << ptId << ".");
+                    return ret;
+                }
+            }
+        }
+        if (negoNum != 0) {
+            LOG_DEBUG("Reset ptId:" << ptId << ", groupIndex:" << groupIndex << ", seq no:" <<
+                      negoSeqList[negoNum - NO_1]);
+            mSequence->ResetSeqNoState2Mst(ptId, groupIndex, negoSeqList[negoNum - NO_1]);
+        }
+    }
+
+    mCm->UpdatePtState(ptId);
+
+    return MMS_OK;
+}
+
+BResult MmsKvServer::GetSeqNoList(uint64_t seqList[], uint32_t &seqNum, uint16_t ptId, uint64_t ptv,
+                                  uint16_t groupIndex, uint16_t nid)
+{
+    uint16_t localId = mCm->GetLocalNid();
+    if (nid == localId) {
+        return mSequence->GetSeqNoList2Slv(ptId, groupIndex, seqList, seqNum);
+    }
+
+    GetSeqListRequest req;
+    GetSeqListResponse rsp;
+
+    req.head = { 0, MMS_OP_S_GET_SEQNO_LIST, groupIndex, ptId, ptv };
+
+    auto ret = mNetEngine->SyncCall<GetSeqListRequest, GetSeqListResponse>(nid, groupIndex, MMS_OP_S_GET_SEQNO_LIST,
+                                                                           req, rsp);
+    if (UNLIKELY(ret != MMS_OK)) {
+        LOG_ERROR("Get seq list failed, ret: " << ret << ", remote:" << nid << ", ptId:" << ptId << ".");
+        return ret;
+    }
+
+    if (UNLIKELY(rsp.seqNum > SEQ_QUEUE_LEN)) {
+        LOG_ERROR("Invalid seqNum:" << rsp.seqNum << ", must be less than " << SEQ_QUEUE_LEN << ".");
+        return MMS_INVALID_PARAM;
+    }
+
+    for (uint32_t i = 0; i < rsp.seqNum; i++) {
+        seqList[i] = rsp.seqList[i];
+        LOG_DEBUG("Get remote:" << nid << ", seq no:" << seqList[i]);
+    }
+    seqNum = rsp.seqNum;
+    return MMS_OK;
+}
+
+BResult MmsKvServer::MergeSeqNoList(uint64_t negoSeqList[], uint16_t negoLocList[], uint32_t &negoSeqNum,
+                                    uint64_t seqList[], uint32_t seqNum, uint16_t remoteId)
+{
+    uint64_t mergeSeqList[SEQ_QUEUE_LEN_T] = { 0 };
+    uint16_t mergeLocList[SEQ_QUEUE_LEN_T] = { 0 };
+    uint32_t mergeNum = 0;
+    uint32_t i = 0;
+    uint32_t j = 0;
+
+    while (i < negoSeqNum && j < seqNum) {
+        if (negoSeqList[i] < seqList[j]) {
+            if (mergeNum == 0 || mergeSeqList[mergeNum - NO_1] != negoSeqList[i]) {
+                mergeSeqList[mergeNum] = negoSeqList[i];
+                mergeLocList[mergeNum] = negoLocList[i];
+                mergeNum++;
+            }
+            i++;
+        } else if (negoSeqList[i] > seqList[j]) {
+            if (mergeNum == 0 || mergeSeqList[mergeNum - NO_1] != seqList[j]) {
+                mergeSeqList[mergeNum] = seqList[j];
+                mergeLocList[mergeNum] = remoteId;
+                mergeNum++;
+            }
+            j++;
+        } else {
+            if (mergeNum == 0 || mergeSeqList[mergeNum - NO_1] != negoSeqList[i]) {
+                mergeSeqList[mergeNum] = negoSeqList[i];
+                mergeLocList[mergeNum] = negoLocList[i];
+                mergeNum++;
+            }
+            i++;
+            j++;
+        }
+    }
+    while (i < negoSeqNum) {
+        if (mergeNum == 0 || mergeSeqList[mergeNum - NO_1] != negoSeqList[i]) {
+            mergeSeqList[mergeNum] = negoSeqList[i];
+            mergeLocList[mergeNum] = negoLocList[i];
+            mergeNum++;
+        }
+        i++;
+    }
+    while (j < seqNum) {
+        if (mergeNum == 0 || mergeSeqList[mergeNum - NO_1] != seqList[j]) {
+            mergeSeqList[mergeNum] = seqList[j];
+            mergeLocList[mergeNum] = remoteId;
+            mergeNum++;
+        }
+        j++;
+    }
+
+    if (mergeNum >= SEQ_QUEUE_LEN) {
+        LOG_WARN("Impossible, Out of range, merge seq no:" << mergeNum);
+        return MMS_ERR;
+    }
+
+    for (i = 0; i < mergeNum; i++) {
+        negoSeqList[i] = mergeSeqList[i];
+        negoLocList[i] = mergeLocList[i];
+        LOG_DEBUG("Merge remote:" << remoteId << ", loc:" << negoLocList[i] << ", seq no:" << negoSeqList[i]);
+    }
+    negoSeqNum = mergeNum;
+    return MMS_OK;
+}
+
+BResult MmsKvServer::SyncSeqNoData(uint64_t negoSeqNo, uint16_t negoLocId, uint64_t seqList[], uint32_t seqNum,
+                                   uint16_t remoteId, CmPtInfo &ptInfo, uint16_t groupIndex)
+{
+    for (uint32_t i = 0; i < seqNum; i++) {
+        if (seqList[i] == negoSeqNo) {
+            return MMS_OK;
+        }
+    }
+
+    void *data;
+    uint32_t len;
+
+    LOG_DEBUG("Sync from:" << negoLocId << ", to:" << remoteId << ", seq no:" << negoSeqNo);
+
+    auto ret = GetSeqNoData(negoSeqNo, negoLocId, ptInfo, groupIndex, &data, len);
+    if (ret != MMS_OK) {
+        return ret;
+    }
+
+    ret = PutSeqNoData(negoSeqNo, negoLocId, ptInfo, groupIndex, data, len);
+    if (ret != MMS_OK) {
+        free(data);
+        return ret;
+    }
+
+    free(data);
+    return MMS_OK;
+}
+
+BResult MmsKvServer::GetSeqNoData(uint64_t negoSeqNo, uint16_t negoLocId, CmPtInfo &ptInfo, uint16_t groupIndex,
+                                  void **data, uint32_t &len)
+{
+    uint16_t localId = mCm->GetLocalNid();
+    if (negoLocId == localId) {
+        uint32_t ioLen = mIoCtxBuffLen;
+        void *ioBuff = malloc(ioLen);
+        if (ioBuff == nullptr) {
+            return MMS_ALLOC_FAIL;
+        }
+        auto ret = mSequence->GetSeqNoData2Slv(ptInfo.ptId, groupIndex, negoSeqNo, ioBuff, ioLen);
+        if (ret != MMS_OK) {
+            free(ioBuff);
+            return ret;
+        }
+        *data = ioBuff;
+        len = ioLen;
+        return MMS_OK;
+    }
+
+    GetSeqDataRequest req;
+    req.head = { 0, MMS_OP_S_GET_SEQNO_DATA, groupIndex, ptInfo.ptId, ptInfo.version };
+    req.seqNo = negoSeqNo;
+    char *respData = nullptr;
+    uint64_t respLen = 0;
+    auto ret = mNetEngine->SyncCall<GetSeqDataRequest, char>(negoLocId, groupIndex, MMS_OP_S_GET_SEQNO_DATA,
+                                                             req, &respData, respLen);
+    if (UNLIKELY(ret != MMS_OK)) {
+        LOG_ERROR("Get seq data failed, ret: " << ret << ", ptId:" << ptInfo.ptId << ".");
+        return ret;
+    }
+    *data = respData;
+    len = static_cast<uint32_t>(respLen);
+    return MMS_OK;
+}
+
+BResult MmsKvServer::PutSeqNoData(uint64_t negoSeqNo, uint16_t negoLocId, CmPtInfo &ptInfo, uint16_t groupIndex,
+                                  void *data, uint32_t len)
+{
+    uint16_t localId = mCm->GetLocalNid();
+
+    IoDataRequest *req = static_cast<IoDataRequest *>(data);
+    req->head = { 0, req->head.opcode, groupIndex, ptInfo.ptId, ptInfo.version };
+    req->seqNo = negoSeqNo;
+    req->negoSeqNo = negoSeqNo - NO_1;
+
+    if (negoLocId == localId) {
+        if (req->head.opcode == MMS_OP_S_PUT) {
+            return PutLocal(data, len);
+        } else if (req->head.opcode == MMS_OP_S_UPDATE) {
+            return UpdateLocal(data, len);
+        } else if (req->head.opcode == MMS_OP_S_DELETE) {
+            return DeleteLocal(data, len);
+        } else {
+            LOG_ERROR("Impossible, invalid opcode:" << req->head.opcode);
+            return MMS_ERR;
+        }
+    }
+
+    BResult resp;
+    auto ret = mNetEngine->SyncCall<BResult>(negoLocId, groupIndex, req->head.opcode, data, len, resp);
+    if (UNLIKELY(ret != MMS_OK || resp != MMS_OK)) {
+        LOG_ERROR("Put seq data failed, ret: " << ret << ", ptId:" << ptInfo.ptId << ".");
+        return ret;
+    }
+
+    return MMS_OK;
+}
+
+BResult MmsKvServer::HandleGetSeqNoList(ServiceContext &ctx)
+{
+    if (UNLIKELY(ctx.MessageDataLen() != sizeof(GetSeqListRequest)) || UNLIKELY(ctx.MessageData() == nullptr)) {
+        LOG_ERROR("Receive message len:" << ctx.MessageDataLen() << " or message data invalid.");
+        mNetEngine->Reply(ctx, MMS_INVALID_PARAM, nullptr, 0);
+        return MMS_OK;
+    }
+
+    GetSeqListRequest *req = static_cast<GetSeqListRequest *>(ctx.MessageData());
+    GetSeqListResponse rsp;
+    auto ret = mSequence->GetSeqNoList2Slv(req->head.ptId, req->head.groupIndex, rsp.seqList, rsp.seqNum);
+    if (ret != MMS_OK) {
+        LOG_ERROR("Get seq no list failed, ret:" << ret << ", ptId:" << req->head.ptId << ", groupIndex:" <<
+                  req->head.groupIndex << ".");
+        mNetEngine->Reply(ctx, MMS_INNER_ERR, nullptr, 0);
+    } else {
+        mNetEngine->Reply(ctx, MMS_OK, &rsp, sizeof(GetSeqListResponse));
+    }
+    return MMS_OK;
+}
+
+BResult MmsKvServer::HandleGetSeqNoData(ServiceContext &ctx)
+{
+    if (UNLIKELY(ctx.MessageDataLen() != sizeof(GetSeqDataRequest)) || UNLIKELY(ctx.MessageData() == nullptr)) {
+        LOG_ERROR("Receive message len:" << ctx.MessageDataLen() << " or message data invalid.");
+        mNetEngine->Reply(ctx, MMS_INVALID_PARAM, nullptr, 0);
+        return MMS_OK;
+    }
+
+    GetSeqDataRequest *req = static_cast<GetSeqDataRequest *>(ctx.MessageData());
+    uint32_t rspLen = mIoCtxBuffLen;
+    void *rspData = malloc(rspLen);
+    if (UNLIKELY(rspData == nullptr)) {
+        LOG_ERROR("Malloc data fail, len:" << rspLen);
+        mNetEngine->Reply(ctx, MMS_ALLOC_FAIL, nullptr, 0);
+        return MMS_OK;
+    }
+    auto ret = mSequence->GetSeqNoData2Slv(req->head.ptId, req->head.groupIndex, req->seqNo, rspData, rspLen);
+    if (ret != MMS_OK) {
+        LOG_ERROR("Get seq no data failed, ret:" << ret << ", ptId:" << req->head.ptId << ", groupIndex:" <<
+                  req->head.groupIndex << ", seq no:" << req->seqNo << ".");
+        mNetEngine->Reply(ctx, MMS_INNER_ERR, nullptr, 0);
+    } else {
+        mNetEngine->Reply(ctx, MMS_OK, rspData, rspLen);
+    }
+    free(rspData);
+    return MMS_OK;
+}
+
+BResult MmsKvServer::HandleUpdatePtVersion(ServiceContext &ctx)
+{
+    if (UNLIKELY(ctx.MessageDataLen() != sizeof(BasicRequest)) || UNLIKELY(ctx.MessageData() == nullptr)) {
+        LOG_ERROR("Receive message len:" << ctx.MessageDataLen() << " or message data invalid.");
+        mNetEngine->Reply(ctx, MMS_INVALID_PARAM, nullptr, 0);
+        return MMS_OK;
+    }
+
+    uint64_t ptVersion = mCm->GetPtVersion();
+    UpdatePtVRsp rsp = {ptVersion};
+    mNetEngine->Reply(ctx, MMS_OK, &rsp, sizeof(UpdatePtVRsp));
+    return MMS_OK;
+}
+
+}
+}
+
