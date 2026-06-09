b766b5fe36d428c670121a2c34761ec35db29912
jianglinjiao <947757466@qq.com>
2026-06-09 19:39:32 +0800

    cleancode

--- ubsio-boostio/src/cache/read/rcache_evict.cpp
+++ ubsio-boostio/src/cache/read/rcache_evict.cpp
@@ -134,8 +134,8 @@ BResult RCacheEvict::Initialize()
             } else {
                 LOG_ERROR("Create thread for read cache evict failed");
                 delete para;
-                RecycleThreadResources(tier);
                 workStatus.store(false);
+                RecycleThreadResources(tier);
                 return BIO_ALLOC_FAIL;
             }
         }
